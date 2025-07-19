165c23edfd024e8133cb1310ac6e8f1965aa756c
JiWon Kim <andrewkimswe@gmail.com>
2025-07-19 14:35:19 +0900

    Initial commit of JoyCrew backend project structure

---
+++ src/main/java/com/joycrew/backend/JoyCrewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.joycrew.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoyCrewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoyCrewBackendApplication.class, args);
+	}
+
+}
